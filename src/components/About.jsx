93ace202d4d6a76033a188a2352d114c5019d64f
Himanshu_Pokhariya <156579564+Himanxhupokhariya@users.noreply.github.com>
2025-05-03 09:05:26 +0530

    main

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Tilt from "react-tilt";
+import Tilt from "react-tilt/dist/Tilt";
+
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
